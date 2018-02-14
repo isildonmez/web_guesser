6126def7ea3ce6fcd655b2dff5ba26b112c8f85e
Isil Donmez <isil.donmez@gmail.com>
2018-02-14 17:40:58 +0000

    Add cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,13 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-@@secret_number = rand(101)
-@@health = 5
+def start
+  @@secret_number = rand(101)
+  @@health = 5
+end
 
 def check_guess(guess)
   return "Way too low!" if (@@secret_number - guess) > 25
   return "Too low!" if (@@secret_number > guess)
-  return "Correct!\nThe secret number is #{@@secret_number}" if @@secret_number == guess
+  return "Correct!\nThe secret number is #{@@secret_number}." if @@secret_number == guess
   return "Too high!" if (guess - @@secret_number) < 25
   return "Way too high!"
 end
@@ -19,11 +21,14 @@ def colouring(guess)
 end
 
 def end_game(guess)
+  if @@secret_number == guess
+    start
+    return "You won!"
+  end
   message = ""
   if (@@health == 1)
-    message = @@secret_number == guess ? "You won!" : "You used all your guesses.\nThe secret number was #{@@secret_number}"
-    @@secret_number = rand(101)
-    @@health = 5
+    message = "You used all your guesses.\nThe secret number was #{@@secret_number}."
+    start
   else
     @@health -= 1
   end
@@ -31,9 +36,13 @@ def end_game(guess)
 end
 
 get '/' do
-  guess = params["guess"].to_i
-  message = check_guess(guess)
-  colour = colouring(guess)
-  result = end_game(guess)
+  if params["cheat"] == "true"
+    message = "The secret number is #{@@secret_number}"
+  else
+    guess = params["guess"].to_i
+    message = check_guess(guess)
+    colour = colouring(guess)
+    result = end_game(guess)
+  end
   erb :index, :locals => {:message => message, :colour => colour, :result => result}
 end
